1dbfbff38df3babb21651270d4ec5aea680765c0
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-04-24 20:12:30 -0300

    Create Estatistico.r

---
+++ Estatistico.r
@@ -0,0 +1,15 @@
+#Teste estatísticos - Correlação e teste T
+#datasets no R
+data () #mostra os datasets disponíveis
+
+View(Orange)
+
+#Correlação
+?cor.test
+cor.test(Orange$circunference,Orange$age)
+
+#Teste T
+$t.test
+# | significa "ou"
+
+laranja <- Otange[Orange$Tree == '1'[Orange$Tree == '2',]]
