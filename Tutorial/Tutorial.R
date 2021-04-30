1b6c32fc78d60fec1b595e9a09cbe4c4bb0c55af
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-04-29 21:00:17 -0300

    Add files via upload

---
+++ Tutorial/Tutorial.R
@@ -0,0 +1,21 @@
+#Integer
+x <- 2L
+typeof(x)
+
+#Double
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3 + 2i
+typeof(z)
+
+#Character
+a <- "h"
+typeof(a)
+
+#logical
+q1 <- T
+typeof(q1)
+q2 <- F
+typeof(q2)
