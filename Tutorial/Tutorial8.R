1ba9dc14772e01830db911dc1fd09657ca10e48b
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-05-03 09:49:38 -0300

    Create Tutorial8.R

---
+++ Tutorial/Tutorial8.R
@@ -0,0 +1,23 @@
+x <- rnorm
+x 
+for(i in x){
+  print(i)
+}
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+print(x[5])
+
+for(j in 1:5){
+  print(i)
+}
+print(x[j])
+
+N <- 100
+a <- rnorm(N)
+b <- rnorm(N)
+
+c <- a * b
+
+d <- rep(NA,N)
